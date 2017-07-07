51b72d4b9d442ed369c1533d955f321cfcca5300
siwulus <siwulus@users.noreply.github.com>
2017-07-07 15:24:21 +0200

    Create IfExercise2.scala

---
+++ IfExercise2.scala
@@ -0,0 +1,13 @@
+object IfExercise2 {
+    
+    def main(args: Array[String]):Unit = {
+        
+        println(divide(10, 11))
+        println(divide(10, 0))
+        
+        
+    }
+    
+    def divide(x: Int, y: Int) = ???
+
+}
