acf80455a7a8413291dc84add593c671ad451e05
siwulus <siwulus@users.noreply.github.com>
2017-08-07 10:15:06 +0200

    Update and rename IfExercise2.scala to divide.scala

---
+++ divide.scala
@@ -0,0 +1,16 @@
+object IfExercise2 {
+    
+    def main(args: Array[String]):Unit = {
+        
+        println(divide(10, 11))
+        println(divide(10, 0))
+        
+        
+    }
+    /**
+    Napisz funkcje ktora przyjmuje dwie liczby calkowite i zwraca wynik ich dzielenia jako liczbe zmiennoprzecinkowa Double.
+    Funkcja powinna obsluzyc przypadek niedozwolonego dzielenia przez 0.
+    */
+    def divide(x: Int, y: Int): Option[Double] = ???
+
+}
